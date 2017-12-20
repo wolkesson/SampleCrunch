9b4d59fb4e6a29cfed813466728587c763c2a4ca
henrik_wolkesson <henrik.wolkesson@gmail.com>
2017-12-21 00:46:38 +0100

    Fixed minimum wait time of Splash screen

--- Sample Crunch/App.xaml.cs
+++ Sample Crunch/App.xaml.cs
@@ -16,7 +16,8 @@ namespace Sample_Crunch
     /// </summary>
     public partial class App : System.Windows.Application
     {
-        private const int MINIMUM_SPLASH_TIME = 1500; // Miliseconds   
+        private const int MINIMUM_SPLASH_TIME = 2000; // Miliseconds   
+        private const int MAXIMUM_SPLASH_TIME = 5000; // Miliseconds  
 
         protected override void OnStartup(System.Windows.StartupEventArgs e)
         {
@@ -25,22 +26,27 @@ namespace Sample_Crunch
             splash.Show();
             splash.Topmost = true;
 
-            // Note: This can not be done before splash because types are registered in ViewModelLocator
+            // Minimum delay so that splash does to disappera too quickly
+            var splashTask = Task.Delay(MINIMUM_SPLASH_TIME);
+
+            // Note: This can not be done before splash loaded because types are registered in 
+            // ViewModelLocator which is loaded by splash (for example)
             var upd = SimpleIoc.Default.GetInstance<ViewModel.UpdateViewModel>();
 
             // Check for updates async
             var updateTask = Task.Run(async () =>
             {
-                await upd.CheckForUpdates(5000);
+                await upd.CheckForUpdates(MAXIMUM_SPLASH_TIME);
             });
 
-            // Load your windows but don't show it yet   
+            // Load main window
             base.OnStartup(e);
             MainWindow main = new MainWindow();
 
-            updateTask.Wait(2000);
+            // Total delay will be between MINIMUM_SPLASH_TIME and MAXIMUM_SPLASH_TIME
+            Task.WaitAll(updateTask, splashTask);
 
-            // Autoclose Splash if no update available
+            // Autoclose Splash if no updates are available
             if (upd.CurrentState == ViewModel.UpdateViewModel.State.Failed ||
                     upd.CurrentState == ViewModel.UpdateViewModel.State.NoUpdateAvailable ||
                     upd.CurrentState == ViewModel.UpdateViewModel.State.Timedout)
