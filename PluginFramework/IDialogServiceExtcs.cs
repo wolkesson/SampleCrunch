4ae58423b93483180545b0ebc1a17772462c4926
wolkesson <5307773+wolkesson@users.noreply.github.com>
2018-12-12 23:12:12 +0100

    Use default browser. Remove internal. (#30)

--- PluginFramework/IDialogServiceExtcs.cs
+++ PluginFramework/IDialogServiceExtcs.cs
@@ -2,7 +2,6 @@
 {
     public interface IDialogServiceExt : GalaSoft.MvvmLight.Views.IDialogService
     {
-        void ShowWebPage(string title, string address);
         IRangeModel ShowRangeDialog(double from, double to);
         string ShowPromptDialog(string prompt, string defaultResponse);
     }
